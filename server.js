239fef447f4c6ab2ecafb2bc1ed31383e21543d8
Luke Kwerneland <lkwerneland@gmail.com>
2018-07-26 20:33:49 -0600

    new push

--- server.js
+++ server.js
@@ -11,23 +11,18 @@ const items = require('./routes/api/items');
 
 const app = express();
 
-// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Config
 const db = require('./config/keys').mongoURI;
 
-// Connect to MongoDB
 mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// Passport middleware
 app.use(passport.initialize());
 
-// Passport Config
 require('./config/passport')(passport);
 
 app.use('/api/users', users);
